057d72663f82ccca7dba3dc3605010d9e618821c
popany <popany@163.com>
2020-09-25 09:31:00 +0000

    add

--- use_log4j/src/main/java/org/example/Log4jDemo.java
+++ use_log4j/src/main/java/org/example/Log4jDemo.java
@@ -1,9 +1,10 @@
 package org.example;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Log4jDemo {
-    private static org.apache.log4j.Logger log = Logger.getLogger(Log4jDemo.class);
+    private static Logger log = LogManager.getLogger(Log4jDemo.class);
 
     public static void main(String[] args) {
 
