0516718524781ea99064e4c8153852d5501c4f4f
popany <popany@163.com>
2020-10-14 07:53:34 +0000

    add

---
+++ use_springboot/use_jdbc/use_hive_jdbc/src/main/java/com/example/use_hive_jdbc/UseHiveJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.use_hive_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseHiveJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseHiveJdbcApplication.class, args);
+	}
+
+}
---
+++ use_springboot/use_jdbc/use_hive_jdbc/src/test/java/com/example/use_hive_jdbc/UseHiveJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.use_hive_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseHiveJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
