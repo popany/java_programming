ca8f16b7bae46d97568d7928bc44aa9f9b942be4
popany <popany@163.com>
2020-11-27 07:23:01 +0000

    add

---
+++ use_spring/dependency_injection_xml/src/main/java/org/example/AppDependencyInjectionXml.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppDependencyInjectionXml 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
