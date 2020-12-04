a745fb88c77e07e0438a78d0cc8c16caf60007b7
popany <popany@163.com>
2020-12-04 11:04:46 +0000

    add

---
+++ use_spring/xml_based_aop/src/main/java/org/example/Bar.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Bar {
+
+    public void needBefore() {
+        System.out.println("needBefore");
+    }
+
+    public void needAfter() {
+        System.out.println("needAfter");
+    }
+    
+    public void needAround() {
+        System.out.println("needAround");
+    }
+
+    public void needBeforeAfterAround() {
+        System.out.println("needBeforeAfterAround");
+    }
+
+    public String needAfterThrowing(int x) throws Exception {
+        System.out.println("needAfterThrowing");
+        if (x == 1) {
+            throw new Exception("throwing");
+        }
+        return "returning2";
+    }
+
+    public String needAfterReturning() {
+        System.out.println("needAfterReturning");
+        return "returning";
+    }
+}
---
+++ use_spring/xml_based_aop/src/main/java/org/example/Foo.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Foo {
+    void func1Before() {
+        System.out.println("before: func1()");
+    }
+
+    void func2After() {
+        System.out.println("after: func2()");
+    }
+
+    void func3Around() {
+        System.out.println("around: func3()");
+    }
+
+    void func4AfterThrowing(Exception ex) {
+        System.out.println("after-throwing: func4(), exception: " + ex.getMessage());
+    }
+
+    void func5AfterReturning(Object obj) {
+        System.out.println("after-returning: func5(), return: " + obj.toString());
+    }
+}
