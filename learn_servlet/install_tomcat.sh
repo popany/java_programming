c77fa8c7f2537c6ddad307d6ce65125020e15a49
popany <popany@163.com>
2020-12-16 03:17:57 +0000

    mod

--- learn_servlet/install_tomcat.sh
+++ learn_servlet/install_tomcat.sh
@@ -2,11 +2,11 @@
 
 TOMCAT_TAR=apache-tomcat-8.5.60.tar.gz
 TOMCAT_DIR=apache-tomcat-8.5.60
-TOMCAT_TAR_URL=https://downloads.apache.org/tomcat/tomcat-8/v8.5.60/bin/apache-tomcat-8.5.60.tar.gz
+TOMCAT_TAR_URL=https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.60/bin/apache-tomcat-8.5.60.tar.gz
 
 install_tomcat() {
     if [[ ! -f "$TOMCAT_TAR" ]]; then
-        curl -o "$TOMCAT_TAR" "$TOMCAT_TAR_URL"
+        wget "$TOMCAT_TAR_URL"
     fi
 
     tar -xvzf "$TOMCAT_TAR"
