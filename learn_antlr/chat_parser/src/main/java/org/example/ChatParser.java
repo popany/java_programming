fc7e53adea6d8c9dee3afd4676c284f3b47d7587
popany <popany@163.com>
2020-12-17 17:14:45 +0000

    add

---
+++ learn_antlr/chat_parser/src/main/java/org/example/ChatParser.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class ChatParser
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- learn_antlr/simple_parser/src/main/java/org/example/AppSimpleParser.java
+++ learn_antlr/simple_parser/src/main/java/org/example/AppSimpleParser.java
@@ -9,7 +9,7 @@ import org.example.SimpleParser.SimpleParserParser;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
-public class App 
+public class AppSimpleParser 
 {
     public static void main( String[] args )
     {
