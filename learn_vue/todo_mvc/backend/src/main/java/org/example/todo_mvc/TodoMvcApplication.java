e5cda94a9245b4fe81ddd8aaa3cdb2269c9bcd57
popany <popany@163.com>
2021-05-24 10:39:39 +0000

    add

---
+++ learn_vue/todo_mvc/backend/src/main/java/org/example/todo_mvc/TodoMvcApplication.java
@@ -0,0 +1,13 @@
+package org.example.todo_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoMvcApplication.class, args);
+	}
+
+}
---
+++ learn_vue/todo_mvc/backend/src/test/java/org/example/todo_mvc/TodoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todo_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
