2618a038ff9990b17e72e41da7786ddc8e46ac46
popany <popany@163.com>
2021-05-27 14:49:55 +0000

    add

--- learn_vue/todo_mvc/backend/src/main/resources/db/data.sql
+++ learn_vue/todo_mvc/backend/src/main/resources/db/data.sql
@@ -1,6 +1,4 @@
-DELETE FROM t_todo;
-
 INSERT INTO t_todo (id, title, completed, create_time) VALUES
 (1, 'task-1', TRUE, CURRENT_TIMESTAMP()),
 (2, 'task-2', FALSE, CURRENT_TIMESTAMP()),
-(3, 'task-3', FALSE, CURRENT_TIMESTAMP());
+(3, 'task-3', FALSE, CURRENT_TIMESTAMP()) ON DUPLICATE KEY UPDATE create_time=CURRENT_TIMESTAMP();
