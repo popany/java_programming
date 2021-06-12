c8bcbe9c670329ffb6a16206b87e3a4ee3b9f0f7
popany <popany@163.com>
2021-06-12 04:25:06 +0000

    add

--- use_pom/demo-foo/foo-server/src/main/java/org/example/foo/server/master/MasterServer.java
+++ use_pom/demo-foo/foo-server/src/main/java/org/example/foo/server/master/MasterServer.java
@@ -1,3 +1,9 @@
+package org.example.foo.server.master;
+
 public class MasterServer {
+
+    public static void main(String[] args) {
+        ;
+    }
     
 }
--- use_pom/demo-foo/foo-server/src/main/java/org/example/foo/server/worker/WorkerServer.java
+++ use_pom/demo-foo/foo-server/src/main/java/org/example/foo/server/worker/WorkerServer.java
@@ -1,3 +1,9 @@
+package org.example.foo.server.worker;
+
 public class WorkerServer {
+    
+    public static void main(String[] args) {
+        ;
+    }
     
 }
