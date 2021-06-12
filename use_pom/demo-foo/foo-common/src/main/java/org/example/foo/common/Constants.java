aafce59ed03e506159ad40bf5fc3f92846c79dea
popany <popany@163.com>
2021-06-12 04:06:40 +0000

    ad

---
+++ use_pom/demo-foo/foo-common/src/main/java/org/example/foo/common/Constants.java
@@ -0,0 +1,7 @@
+package org.example.foo.common;
+
+public final class Constants {
+    private Constants() {
+        throw new IllegalStateException("Constants class");
+    }
+}
---
+++ use_pom/demo-foo/foo-common/src/test/java/org/example/foo/common/ConstantsTest.java
@@ -0,0 +1,5 @@
+package org.example.foo.common;
+
+public class ConstantsTest {
+    
+}
