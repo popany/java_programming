c580a98a94358fd998c2cdea66f256179b34c8fa
popany <popany@163.com>
2021-06-13 03:20:01 +0000

    add

--- use_pom/demo-foo/foo-server/src/test/java/org/example/foo/server/worker/WorkerServerTest.java
+++ use_pom/demo-foo/foo-server/src/test/java/org/example/foo/server/worker/WorkerServerTest.java
@@ -1,3 +1,12 @@
+package org.example.foo.server.worker;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
 public class WorkerServerTest {
-    
+    @Test
+    public void getString() {
+        assertNotNull("");
+    }   
 }
